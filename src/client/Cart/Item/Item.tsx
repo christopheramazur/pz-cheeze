4e44542cdce7720a08a0b78316758336e53fba77
mazur <chris.mazur@wtfast.com>
2022-04-07 22:06:45 +1000

    Used documentation from https://mui.com/getting-started/usage/ to begin adding on-click functionality to the grid items

--- src/client/Cart/Item/Item.tsx
+++ src/client/Cart/Item/Item.tsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+
+
 // Types
 import { CartItemType } from '../../App';
+
 // Styles
 import { Wrapper } from './Item.styles';
 
@@ -9,17 +18,26 @@ type Props = {
   handleAddToCart: (clickedItem: CartItemType) => void;
 };
 
-const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
-  <Wrapper>
-    <img src={item.image} alt={item.title} />
-    <div>
-      <h3>{item.title}</h3>
-      <h3>${item.price}</h3>
-    </div>
-    <Button
-      onClick={() => handleAddToCart(item)}
-      data-cy={`add-to-cart-${item.id}`}>Add to cart</Button>
-  </Wrapper>
-);
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => {
+  // https://mui.com/components/dialogs/ -- add a dialog to the item wrapper
+  const [dialogVisible, setDialogVisible] = useState(false);
 
+  return (
+    <Wrapper>
+
+      <img src={item.image} alt={item.title} onClick={() => setDialogVisible(true)} />
+      <div>
+        <h3>{item.title}</h3>
+        <h3>${item.price}</h3>
+      </div>
+      <Button
+        onClick={() => handleAddToCart(item)}
+        data-cy={`add-to-cart-${item.id}`}>Add to cart</Button>
+      
+      <Dialog open={dialogVisible} onClose={() => setDialogVisible(false)}>
+        <DialogTitle>{item.title}</DialogTitle>
+      </Dialog>
+    </Wrapper>
+  );
+}
 export default Item;
